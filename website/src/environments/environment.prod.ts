2f40e65dafc8cdde8dd1da07808ff0deaf1862c4
Chris Rodriguez <christopher.eugene.rodriguez@gmail.com>
2023-12-31 02:17:31 -0800

    prod

--- website/src/environments/environment.prod.ts
+++ website/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  mobile: false,
+  stripeKey: 'pk_test_W1SinosLrjqAYGojim3RoDbj00xojm9kng',
+  isServer: true,
+  host: 'https://grceri.com',
+  googleClientId: '1096068654354-9eg92nrkfcfikpe8uqalk797dchfhcf8.apps.googleusercontent.com'
+
 };
